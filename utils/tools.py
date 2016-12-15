3629cfdcba2bd66b6be0b0a526ebd5b0cf2bcea2
boris.liu <boris_liu@foxmail.com>
2016-12-15 16:41:51 +0800

    remote selenium
    Change-Id: Ib8fac3ee25388ba03db5c204d5c5cb69e620ee02

--- utils/tools.py
+++ utils/tools.py
@@ -16,7 +16,7 @@ import time
 import os
 import execjs
 # pip install PyExecJS
-from selenium import webdriver
+# from selenium import webdriver
 import time
 
 def getHtml(url, code = ''):
@@ -357,42 +357,42 @@ def downloadFile(url, basePath, filename, callFunc = ''):
     else:
         return 0
 
-def capture(url, save_fn="capture.png"):
-    directory = os.path.dirname(save_fn)
-    mkdir(directory)
+# def capture(url, save_fn="capture.png"):
+#     directory = os.path.dirname(save_fn)
+#     mkdir(directory)
 
-    browser = webdriver.PhantomJS(executable_path = 'D:/software/phantomjs-2.1.1-windows/phantomjs-2.1.1-windows/bin/phantomjs')
-    browser.set_window_size(1200, 900)
-    browser.get(url) # Load page
-    browser.execute_script("""
-            (function () {
-              var y = 0;
-              var step = 100;
-              window.scroll(0, 0);
+#     browser = webdriver.PhantomJS(executable_path = 'D:/software/phantomjs-2.1.1-windows/phantomjs-2.1.1-windows/bin/phantomjs')
+#     browser.set_window_size(1200, 900)
+#     browser.get(url) # Load page
+#     browser.execute_script("""
+#             (function () {
+#               var y = 0;
+#               var step = 100;
+#               window.scroll(0, 0);
 
-              function f() {
-                if (y < document.body.scrollHeight) {
-                  y += step;
-                  window.scroll(0, y);
-                  setTimeout(f, 50);
-                } else {
-                  window.scroll(0, 0);
-                  document.title += "scroll-done";
-                }
-              }
+#               function f() {
+#                 if (y < document.body.scrollHeight) {
+#                   y += step;
+#                   window.scroll(0, y);
+#                   setTimeout(f, 50);
+#                 } else {
+#                   window.scroll(0, 0);
+#                   document.title += "scroll-done";
+#                 }
+#               }
 
-              setTimeout(f, 1000);
-            })();
-        """)
+#               setTimeout(f, 1000);
+#             })();
+#         """)
 
-    for i in range(30):
-        if "scroll-done" in browser.title:
-            break
-        time.sleep(1)
+#     for i in range(30):
+#         if "scroll-done" in browser.title:
+#             break
+#         time.sleep(1)
 
-    browser.save_screenshot(save_fn)
-    browser.close()
+#     browser.save_screenshot(save_fn)
+#     browser.close()
 
-if __name__ == "__main__":
-   # capture("http://www.jb51.net", '../dd.png')
-   downloadFile('http://img.lzep.cn/ep/other/ckplayer6.5/ckplayer.swf', '.', 'video.swf')
+# if __name__ == "__main__":
+#    # capture("http://www.jb51.net", '../dd.png')
+#    downloadFile('http://img.lzep.cn/ep/other/ckplayer6.5/ckplayer.swf', '.', 'video.swf')
